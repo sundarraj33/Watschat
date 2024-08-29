4fc2e7ceab0c85abc40cd511963e2d9015cc2140
sundarraj33 <sundarraj.m033@gmail.com>
2024-08-29 23:47:06 +0530

    watschar

--- src/Candidate/Register.js
+++ src/Candidate/Register.js
@@ -5,8 +5,18 @@ const Register = ()=>{
             <div className="container">
                 <div className="wrapper border border-dark">
                     <div className="box border border-dark">
-                        <label>Name</label>
-                        <input type=''></input>
+                       <div className='input-filed'>
+                            <label>Name</label>
+                            <input type='' className='border border-0'></input>
+                        </div>
+                        <div className='input-filed'>
+                            <label>Mobile</label>
+                            <input type='' className='border border-0 inputfield border-bottom'></input>
+                        </div>
+                        <div className='input-filed'>
+                            <label>Name</label>
+                            <input type='' className='border border-0'></input>
+                        </div>
                     </div>
                 </div>
             </div>
