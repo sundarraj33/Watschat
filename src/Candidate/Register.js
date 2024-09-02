c50c6c3f128bc4d35eb5222903689c89cb3d6c4c
sundarraj33 <sundarraj.m033@gmail.com>
2024-09-02 21:05:31 +0530

    Sample page

--- src/Candidate/Register.js
+++ src/Candidate/Register.js
@@ -4,18 +4,18 @@ const Register = ()=>{
         <>
             <div className="container">
                 <div className="wrapper border border-dark">
-                    <div className="box border border-dark">
+                    <div className="box border border-dark " style={{'width':'100%','background-color':'#0000FF'}}>
                        <div className='input-filed'>
                             <label>Name</label>
-                            <input type='' className='border border-0'></input>
+                            <input type='' className='border border-0 inputfield border-bottom' maxlength="30"></input>
                         </div>
                         <div className='input-filed'>
                             <label>Mobile</label>
-                            <input type='' className='border border-0 inputfield border-bottom'></input>
+                            <input type='' maxlength="10" className='border border-0 inputfield border-bottom'></input>
                         </div>
                         <div className='input-filed'>
                             <label>Name</label>
-                            <input type='' className='border border-0'></input>
+                            <input type='' className='border border-0 inputfield border-bottom' ></input>
                         </div>
                     </div>
                 </div>
