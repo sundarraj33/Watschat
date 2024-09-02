d598f82e70c7495a79d6c5b5baea77885a76952b
sundarraj33 <sundarraj.m033@gmail.com>
2024-09-02 21:36:46 +0530

    Sample page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import Register from "./Candidate/Register";
-import Login from "./Candidate/Login";
+// import Register from "./Candidate/Register";
+// import Login from "./Candidate/Login";
+import Main_chat from './Chat/Main_chat';
 import './App.css';
 function App() {
   return (
     <div className="App">
       {/* <Register /> */}
-    <Login />
+    {/* <Login /> */}
+    <Main_chat />
     </div>
   );
 }
---
+++ src/Chat/Main_chat.js
@@ -0,0 +1,16 @@
+const Main_chat = ()=>{
+    return(
+        <>
+        <div className="container">
+            <div className="header_chat">
+                <img src="https://bebloggerworld.wordpress.com/wp-content/uploads/2018/02/lord-shiva-images-on-maha-shivaratri-whatsapp-dp-facebook-cover-page-profile-pic.jpg?w=564" className="chat_img"></img>
+                <div className="heading-name">
+                    <label className="text-light">Sundar</label>
+                </div>
+            </div>
+            </div>
+        </>
+    )
+}
+
+export default Main_chat;
